a3c3c7da6e620b85c81e690b3c8b09b5473e8e9c
Matthew Flynn <mpflynnx@gmail.com>
2025-09-03 10:06:32 +0000

    Added v7 of hello() function Added test for new language parameter Added new parameter language

---
+++ hello-world/v7/hello_test.go
@@ -0,0 +1,30 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to the given name in English if language parameter is empty", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World` when Hello() parameter name is empty", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello in Spanish", func(t *testing.T) {
+		got := Hello("Maria", "Spanish")
+		want := "Hola, Maria"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
